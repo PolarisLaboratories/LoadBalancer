fdde5cb9490c8b47dc73294f1172aae9e97b4505
Jason Lu <jasonbangbang@gmail.com>
2017-05-09 11:28:13 -0500

    Add support for dev website

--- install-generic.sh
+++ install-generic.sh
@@ -11,15 +11,22 @@ DIR=$1
 PREFIX=$2
 SUDO=$3
 
+if [[ $DEV -eq 1 ]]
+then
+    WEBSITE="dev.paladin.polarislabs.io"
+else
+    WEBSITE="paladin.polarislabs.io"
+fi
+
 $SUDO cp -p $DIR/nginx.conf "$PREFIX"etc/nginx/
 
-if $SUDO test -f "/etc/letsencrypt/live/paladin.polarislabs.io/fullchain.pem"
+if $SUDO test -f "/etc/letsencrypt/live/$WEBSITE/fullchain.pem"
 then
     echo "LetsEncrypt cert detected. Symlinking into $PRERFIX/etc/nginx"
     $SUDO rm "$PREFIX"etc/nginx/server.crt 2&> /dev/null
     $SUDO rm "$PREFIX"etc/nginx/server.key 2&> /dev/null
-    $SUDO ln -s /etc/letsencrypt/live/paladin.polarislabs.io/fullchain.pem "$PREFIX"etc/nginx/server.crt
-    $SUDO ln -s /etc/letsencrypt/live/paladin.polarislabs.io/privkey.pem "$PREFIX"etc/nginx/server.key
+    $SUDO ln -s /etc/letsencrypt/live/$WEBSITE/fullchain.pem "$PREFIX"etc/nginx/server.crt
+    $SUDO ln -s /etc/letsencrypt/live/$WEBSITE/privkey.pem "$PREFIX"etc/nginx/server.key
 else
     echo "LetsEncrypt cert NOT detected. Installing self-signed certs"
     $SUDO cp -p server.crt "$PREFIX"etc/nginx/
